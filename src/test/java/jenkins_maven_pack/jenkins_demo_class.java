a89bdda9fa98a0ad7557bd2df38e9688e2a2b335
Anu <anu.nandanan@gmail.com>
2025-11-21 14:47:25 +0530

    First commit

---
+++ src/test/java/jenkins_maven_pack/jenkins_demo_class.java
@@ -0,0 +1,12 @@
+package jenkins_maven_pack;
+
+import org.testng.annotations.Test;
+
+public class jenkins_demo_class {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Hellow world!");
+	  
+  }
+}
